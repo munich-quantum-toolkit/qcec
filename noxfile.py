60c120199d26b4937d2f366e44f5a3308b20f242
Daniel Haag <denialhaag@gmail.com>
2025-07-29 19:37:05 +0200

    Do not rebuild MQT Core in tests

--- noxfile.py
+++ noxfile.py
@@ -66,11 +66,6 @@ def _run_tests(
         "build",
         "--only-group",
         "test",
-        # Build mqt-core from source to work around pybind believing that two
-        # compiled extensions might not be binary compatible.
-        # This will be fixed in a new pybind11 release that includes https://github.com/pybind/pybind11/pull/5439.
-        "--no-binary-package",
-        "mqt-core",
         "--verbose",
         *install_args,
         env=env,
@@ -151,11 +146,6 @@ def docs(session: nox.Session) -> None:
         "build",
         "--only-group",
         "docs",
-        # Build mqt-core from source to work around pybind believing that two
-        # compiled extensions might not be binary compatible.
-        # This will be fixed in a new pybind11 release that includes https://github.com/pybind/pybind11/pull/5439.
-        "--no-binary-package",
-        "mqt-core",
         env=env,
     )
 
