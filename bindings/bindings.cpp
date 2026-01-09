c816c9cb788fc3f67bd99393a46af631bbeb5441
Daniel Haag <121057143+denialhaag@users.noreply.github.com>
2026-01-09 17:24:36 +0100

    Switch to nanobind

--- bindings/bindings.cpp
+++ bindings/bindings.cpp
@@ -8,28 +8,27 @@
  * Licensed under the MIT License
  */
 
-#include <pybind11/pybind11.h>
-#include <pybind11/stl.h> // NOLINT(misc-include-cleaner)
+#include <nanobind/nanobind.h>
 
 namespace ec {
 
-namespace py = pybind11;
-using namespace pybind11::literals;
+namespace nb = nanobind;
+using namespace nb::literals;
 
 // forward declarations
-void registerApplicationSchema(const py::module& mod);
-void registerConfiguration(const py::module& mod);
-void registerEquivalenceCheckingManager(const py::module& mod);
-void registerEquivalenceCriterion(const py::module& mod);
-void registerStateType(const py::module& mod);
+void registerApplicationSchema(const nb::module_& m);
+void registerConfiguration(const nb::module_& m);
+void registerEquivalenceCheckingManager(const nb::module_& m);
+void registerEquivalenceCriterion(const nb::module_& m);
+void registerStateType(const nb::module_& m);
 
 // NOLINTNEXTLINE(misc-include-cleaner)
-PYBIND11_MODULE(MQT_QCEC_MODULE_NAME, mod, py::mod_gil_not_used()) {
-  registerApplicationSchema(mod);
-  registerConfiguration(mod);
-  registerEquivalenceCheckingManager(mod);
-  registerEquivalenceCriterion(mod);
-  registerStateType(mod);
+NB_MODULE(MQT_QCEC_MODULE_NAME, m) {
+  registerApplicationSchema(m);
+  registerConfiguration(m);
+  registerEquivalenceCheckingManager(m);
+  registerEquivalenceCriterion(m);
+  registerStateType(m);
 }
 
 } // namespace ec
--- bindings/equivalence_checking_manager.cpp
+++ bindings/equivalence_checking_manager.cpp
@@ -13,36 +13,35 @@
 #include "EquivalenceCriterion.hpp"
 #include "ir/QuantumComputation.hpp"
 
-#include <pybind11/cast.h>
-#include <pybind11/pybind11.h>
-#include <pybind11/stl.h> // NOLINT(misc-include-cleaner)
+#include <nanobind/nanobind.h>
+#include <nanobind/stl/string.h>      // NOLINT(misc-include-cleaner)
+#include <nanobind/stl/string_view.h> // NOLINT(misc-include-cleaner)
+#include <nlohmann/json.hpp>
 
 namespace ec {
 
-namespace py = pybind11;
-using namespace pybind11::literals;
+namespace nb = nanobind;
+using namespace nb::literals;
 
 // NOLINTNEXTLINE(misc-use-internal-linkage)
-void registerEquivalenceCheckingManager(const py::module& mod) {
+void registerEquivalenceCheckingManager(const nb::module_& m) {
   // Class definitions
   auto ecm =
-      py::class_<EquivalenceCheckingManager>(mod, "EquivalenceCheckingManager");
+      nb::class_<EquivalenceCheckingManager>(m, "EquivalenceCheckingManager");
   auto results =
-      py::class_<EquivalenceCheckingManager::Results>(ecm, "Results");
+      nb::class_<EquivalenceCheckingManager::Results>(ecm, "Results");
 
   // Constructors
-  ecm.def(py::init<const qc::QuantumComputation&, const qc::QuantumComputation&,
+  ecm.def(nb::init<const qc::QuantumComputation&, const qc::QuantumComputation&,
                    Configuration>(),
           "circ1"_a, "circ2"_a, "config"_a = Configuration());
 
   // Access to circuits
-  ecm.def_property_readonly("qc1",
-                            &EquivalenceCheckingManager::getFirstCircuit);
-  ecm.def_property_readonly("qc2",
-                            &EquivalenceCheckingManager::getSecondCircuit);
+  ecm.def_prop_ro("qc1", &EquivalenceCheckingManager::getFirstCircuit);
+  ecm.def_prop_ro("qc2", &EquivalenceCheckingManager::getSecondCircuit);
 
   // Access to configuration
-  ecm.def_property(
+  ecm.def_prop_rw(
       "configuration", &EquivalenceCheckingManager::getConfiguration,
       [](EquivalenceCheckingManager& manager, const Configuration& config) {
         manager.getConfiguration() = config;
@@ -52,9 +51,8 @@ void registerEquivalenceCheckingManager(const py::module& mod) {
   ecm.def("run", &EquivalenceCheckingManager::run);
 
   // Results
-  ecm.def_property_readonly("results", &EquivalenceCheckingManager::getResults);
-  ecm.def_property_readonly("equivalence",
-                            &EquivalenceCheckingManager::equivalence);
+  ecm.def_prop_ro("results", &EquivalenceCheckingManager::getResults);
+  ecm.def_prop_ro("equivalence", &EquivalenceCheckingManager::equivalence);
 
   // Convenience functions
   // Execution
@@ -72,31 +70,36 @@ void registerEquivalenceCheckingManager(const py::module& mod) {
       });
 
   // EquivalenceCheckingManager::Results bindings
-  results.def(py::init<>())
-      .def_readwrite("preprocessing_time",
-                     &EquivalenceCheckingManager::Results::preprocessingTime)
-      .def_readwrite("check_time",
-                     &EquivalenceCheckingManager::Results::checkTime)
-      .def_readwrite("equivalence",
-                     &EquivalenceCheckingManager::Results::equivalence)
-      .def_readwrite("started_simulations",
-                     &EquivalenceCheckingManager::Results::startedSimulations)
-      .def_readwrite("performed_simulations",
-                     &EquivalenceCheckingManager::Results::performedSimulations)
-      .def_readwrite("cex_input",
-                     &EquivalenceCheckingManager::Results::cexInput)
-      .def_readwrite("cex_output1",
-                     &EquivalenceCheckingManager::Results::cexOutput1)
-      .def_readwrite("cex_output2",
-                     &EquivalenceCheckingManager::Results::cexOutput2)
-      .def_readwrite(
-          "performed_instantiations",
-          &EquivalenceCheckingManager::Results::performedInstantiations)
-      .def_readwrite("checker_results",
-                     &EquivalenceCheckingManager::Results::checkerResults)
+  results.def(nb::init<>())
+      .def_rw("preprocessing_time",
+              &EquivalenceCheckingManager::Results::preprocessingTime)
+      .def_rw("check_time", &EquivalenceCheckingManager::Results::checkTime)
+      .def_rw("equivalence", &EquivalenceCheckingManager::Results::equivalence)
+      .def_rw("started_simulations",
+              &EquivalenceCheckingManager::Results::startedSimulations)
+      .def_rw("performed_simulations",
+              &EquivalenceCheckingManager::Results::performedSimulations)
+      .def_rw("cex_input", &EquivalenceCheckingManager::Results::cexInput)
+      .def_rw("cex_output1", &EquivalenceCheckingManager::Results::cexOutput1)
+      .def_rw("cex_output2", &EquivalenceCheckingManager::Results::cexOutput2)
+      .def_rw("performed_instantiations",
+              &EquivalenceCheckingManager::Results::performedInstantiations)
+      .def_prop_rw(
+          "checker_results",
+          [](const EquivalenceCheckingManager::Results& results) {
+            return nb::cast(results.checkerResults);
+          },
+          [](EquivalenceCheckingManager::Results& results,
+             const nb::dict& obj) {
+            std::string jsonString = nb::cast<std::string>(nb::str(obj));
+            results.checkerResults = nlohmann::json::parse(jsonString);
+          })
       .def("considered_equivalent",
            &EquivalenceCheckingManager::Results::consideredEquivalent)
-      .def("json", &EquivalenceCheckingManager::Results::json)
+      .def("json",
+           [](const EquivalenceCheckingManager::Results& results) {
+             return nb::cast(results.json());
+           })
       .def("__str__", &EquivalenceCheckingManager::Results::toString)
       .def("__repr__", [](const EquivalenceCheckingManager::Results& res) {
         return "<EquivalenceCheckingManager.Results: " +
